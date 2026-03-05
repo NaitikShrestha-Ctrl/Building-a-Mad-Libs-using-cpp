3ca58264f3289224eeab26fe56ccd0c8606565d8
Naitik Shrestha <shresthanaitik010@gmail.com>
2026-03-05 20:29:16 +0545

    Mad Libs incomplete

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Building-a-Mad-Libs-using-cpp
-I have made simple mad libs in C++.
+
+In this project, I have simply build Mad Libs coded in C++. The program asks the user to enter different words such as nouns, number, animals, adjective, etc. After collecting the inputs, it generates a short paragraph using those words. 
---
+++ madLibs.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    string string1, string2, string3, string4, string5, string6, string7, string8, string9, string10, string11;
+    int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
+
+    cout << "Enter the number(1 to 12): " ;
+    cin >> num1;
+
+    cout << "Enter you favourite animal: " ;
+    cin >> string1;
+
+    cout <<"Enter adjective:" ;
+    cin >> string2;
+
+    cout << "Enter noun: ";
+    cin >> string3;
+    
+    cout << "Enter number: ";
+    cin >> num2;
+
+    cout << "Enter number:" ;
+    cin >> num3;
+
+    cout << "Enter funny  animal:" ;
+    cin >> string4;
+
+    cout << "Enter number:" ;
+    cin >> num4;
+
+    cout << "Enter number:" ;
+    cin >> num5;
+
+    cout << "Enter object:" ;
+    cin >> string5;
+
+    cout << "Enter number:" ;
+    cin >> num6;
+
+    cout << "Enter  plural animal:" ;
+    cin >> string6;
+
+    cout << "Enter name:" ;
+    cin >> string7;
+
+    cout << "Enter adjective:" ;
+    cin >> string8;
+
+    cout << "Enter number:" ;
+    cin >> num7;
+    
+    cout << "Enter drink name: " ;
+    cin >> string9;
+
+    cout << "Enter number:" ;
+    cin >> num8;
+
+    cout << "Enter number:" ;
+    cin >> num9;
+
+    cout << "Enter food: " ;
+    cin >> string10;
+
+    cout << "Enter adjective:" ;
+    cin >> string11;
+
+    cout << "Enter number: " ;
+    cin >> num10;
+
+    cout << "Today I woke up at " <<num1 <<" in th morning becasue "
+
+    return 0;
+}
