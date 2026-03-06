4471ae7a4ef067e3f15cb0016dbee48c0a820b5a
Naitik Shrestha <shresthanaitik010@gmail.com>
2026-03-06 16:01:27 +0545

    Mad Libs Game Complete Version

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Building-a-Mad-Libs-using-cpp
 
 In this project, I have simply build Mad Libs coded in C++. The program asks the user to enter different words such as nouns, number, animals, adjective, etc. After collecting the inputs, it generates a short paragraph using those words. 
+
+# Script  i wrote
+Today I woke up at 7 in the morning because my cat told me to exercise. I quickly put on my sparkly workout clothes and ran outside with my toaster. First, I ran 3 laps around the park. After the 2nd lap, I was so tired that I started running like a fluffy penguin. Then I tried to do 10 push-ups, but after 4 push-ups I fell down like a giant pancake! Suddenly, 5 squirrels started cheering for me. My friend Alex laughed and said I looked hilarious while exercising. Finally, I drank 2 bottles of orange juice and ate 3 donuts to recover. Now I feel fantastic, but I think next time I will only exercise for 15 minutes!
--- madLibs.cpp
+++ madLibs.cpp
@@ -7,6 +7,9 @@ int main()
     string string1, string2, string3, string4, string5, string6, string7, string8, string9, string10, string11;
     int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
 
+    cout << "Mad Libd Game" << endl;
+    cout << "Answer the question to get funny output later." <<endl;
+
     cout << "Enter the number(1 to 12): " ;
     cin >> num1;
 
@@ -70,7 +73,8 @@ int main()
     cout << "Enter number: " ;
     cin >> num10;
 
-    cout << "Today I woke up at " <<num1 <<" in th morning becasue "
+    cout << "Today I woke up at " <<num1 <<" in the morning becasue my " <<string1 << " told me to exercise. I quickly put on my " <<string2 << " workout clothes and ran outside with my " <<string3 << " ." <<" First, i ran " <<num2 << " lap around the park. After the " <<num3 << " I was so tired that I started running like a " <<string4 << " Then I tried to do " <<num4 << " push-ups, but after " <<num5 
+         <<" push-ups I fell down like a " <<string5 <<"!" <<" Suddenly, " <<num6 <<string6 <<" started cheering for me. My friend " <<string7 << " laughed and said I looked " <<string8 <<" while exercising ." <<" Finally, I drank " <<num7 <<" bottles of " <<string9 <<" and ate " <<num8 <<string10 <<" to recover . Now I feel " << string11 <<" but I think next time I will only exercise for " <<num9 << " minutes." <<endl;
 
     return 0;
 }
